4db524ffa7d5bad75fc254b64c02ed9e279f15fc
unknown <subratoghosh523@gmail.com>
2024-01-01 15:27:41 +0530

    third commit

--- StringRev.cpp
+++ StringRev.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    char name[] = {'S','U','B','R','A','T','O'};
-    cout<<name<<endl;
+int main() {
+    char name[] = {'S', 'U', 'B', 'R', 'A', 'T', 'O'};
+    cout << "Original: " << name << endl;
+
+    for (int i = 0, j = 6; i <= j; i++, j--) {
+        char temp = name[i];
+        name[i] = name[j];
+        name[j] = temp;
+    }
+
+    cout << "Reversed: " << name << endl;
+
+    return 0;
 }
